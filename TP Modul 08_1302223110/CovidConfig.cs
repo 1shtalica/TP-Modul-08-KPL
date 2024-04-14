5a422d37ae5d9e8c0d50720b683ec650fdf90e47
1shtalica <adibfaizulhaq05@gmail.com>
2024-04-14 21:20:31 +0700

    menambahkan kelas covidconfig dan menjalankan program

---
+++ TP Modul 08_1302223110/CovidConfig.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TP_Modul_08_1302223110
+{
+    internal class CovidConfig
+    {
+        public int batas_hari_demam { get; set; }
+        public string pesan_diterima { get; set; }
+        public string pesan_ditolak { get; set; }
+        public string satuan_suhu { get; set; }
+
+        public CovidConfig readJSON()
+        {
+            String filejson = File.ReadAllText(@"E:\\Programming\\Project\\Kuliah\\KPL\\TP Modul 08\\TP Modul 08_1302223110\\TP Modul 08_1302223110\\covid_config.json");
+            CovidConfig covidConfig = JsonConvert.DeserializeObject<CovidConfig>(filejson);
+            return covidConfig;
+        }
+
+        public void UbahSatuan()
+        {
+            if (satuan_suhu == "celcius")
+            {
+                satuan_suhu = "fahrenheit";
+            } else
+            {
+                satuan_suhu = "celcius";
+            }
+        }
+
+        public void CekInputC(double suhu, int hari)
+        {
+            if (satuan_suhu == "celcius")
+            {
+                if (suhu >= 36.5 && suhu <= 37.5 && hari < batas_hari_demam)
+                {
+                    Console.WriteLine(pesan_diterima);
+                }
+                else
+                {
+                    Console.WriteLine(pesan_ditolak);
+
+                }
+            }
+        }
+
+        public void CekInputF(double suhu, int hari)
+        {
+            if (satuan_suhu == "fahrenheit")
+            {
+                if (suhu >= 97.7 && suhu <= 99.5 && hari < batas_hari_demam)
+                {
+                    Console.WriteLine(pesan_diterima);
+                }
+                else
+                {
+                    Console.WriteLine(pesan_ditolak);
+                }
+            }
+        }
+    }
+}
--- TP Modul 08_1302223110/Program.cs
+++ TP Modul 08_1302223110/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,27 @@ namespace TP_Modul_08_1302223110
     {
         static void Main(string[] args)
         {
+            CovidConfig config = new CovidConfig().readJSON();
+
+            Console.WriteLine("Berapa suhu badan anda saat ini? Dalam nilai " + config.satuan_suhu);
+            double inputSuhuC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Berapa hari yang lalu anda terakhir memiliki gejala demam?");
+            int inputHariC = int.Parse(Console.ReadLine());
+
+            config.CekInputC(inputSuhuC, inputHariC);
+
+            config.UbahSatuan();
+
+            Console.WriteLine("Berapa suhu badan anda saat ini? Dalam nilai " + config.satuan_suhu);
+            double inputSuhuF = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Berapa hari yang lalu anda terakhir memiliki gejala demam?");
+            int inputHariF = int.Parse(Console.ReadLine());
+
+            config.CekInputF(inputSuhuF, inputHariF);
+
+            Console.ReadLine();
         }
     }
 }
